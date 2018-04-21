2385cf4b30ab278871a24df814c1d94d54688b0c
Charlie <ncsucharlie@gmail.com>
2018-04-21 13:05:13 -0400

    Fix up runSearch scripts to be executable and run in correct directory

--- bingAutomation/executable-jar/runSearch.bat
+++ bingAutomation/executable-jar/runSearch.bat
@@ -1,2 +1,1 @@
-cd c:/charlie/bingRewards/bing-rewards-executable
 java -jar bingSearch.jar
--- bingAutomation/executable-jar/runSearch.sh
+++ bingAutomation/executable-jar/runSearch.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-MY_DIR="/home/charlie/projects/bing-search/bing-rewards-executable"
+MY_DIR="/home/charlie/github/bingAutomation-eclipse-workspace/bingAutomation/executable-jar"
 cd $MY_DIR
 java -jar bingSearch.jar | tee $MY_DIR/output.log
 date >> output.log
